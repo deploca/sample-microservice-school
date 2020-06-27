e4cb4e058d20f67a10a6737bd48161e9e65f9ed5
Mostafa Rowghanian <mostafatech@outlook.com>
2020-06-27 23:46:05 +0430

    fix publish path for github actions to work

--- publish.sh
+++ publish.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Copy deploy folder contents
-mkdir /tmp/Publish
-cp -r .deploy/* /tmp/publish/
+mkdir ./_publish
+cp -r ./.deploy/* ./_publish/
 
 # Publish Students Api
 cd SampleMicroserviceSchool.Students.Api
-dotnet publish -c Release -o /tmp/publish/student --self-contained false --no-restore
+dotnet publish -c Release -o ./_publish/student --self-contained false
 cd ..
 
 # Publish Income Api
 cd SampleMicroserviceSchool.Income.Api
-dotnet publish -c Release -o /tmp/publish/income --self-contained false --no-restore
+dotnet publish -c Release -o ./_publish/income --self-contained false
 cd ..
 
 # Publish UI
@@ -20,6 +20,6 @@ npm run build
 cd ..
 
 # compress publishes
-zip -r /tmp/publish/deploy.zip /tmp/publish
+#zip -r /tmp/publish/deploy.zip /tmp/publish
 
 echo Publish finished successfully
